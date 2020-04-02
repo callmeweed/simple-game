79f92c063f3638756470d9e7cab2d757170bce81
callmeweed <63060208+callmeweed@users.noreply.github.com>
2020-04-03 02:35:35 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -6,36 +6,89 @@
 
 int main(int argc, char *argv[]) 
 {
-	int soBiMat, mynumber, count, extragame=1;
-	const int MIN=1, MAX=100;
+	int soBiMat, mynumber, count, extragame=1, nguoichoi;
 	srand(time(NULL));
 	printf("Wellcome to my game!!\n\n");
-	printf("My game is very simple!\n");
-	printf("All you have to do what pick a number until correct.\n");
-	printf("Gud luck!\n\n\n");
 	while (extragame==1)
 	{
-		soBiMat=(rand()%(MAX-MIN+1))+MIN;
-	
-		while (mynumber!=soBiMat)
+		printf("1. 1 Player!\n");	
+		printf("2. 2 Players!\n");
+		printf("Your choice: ");
+		scanf("%d", &nguoichoi);
+		printf("\n\nMy game is very simple!\n");
+		if (nguoichoi==2)
 		{
-			count++;
-			printf("Your number: ");
-			scanf("%d", &mynumber);
-			if (soBiMat>mynumber)
+			printf("One person will choose a number and one person will guess!\n");
+			printf("Gud luck!\n\n\n");		
+			printf("First person: ");
+			scanf("%d", &soBiMat);
+			while (mynumber!=soBiMat)
 			{
-				printf("Oh noo, Bigger!!\n\n");
+				count++;
+				printf("\n\nSecond person: ");
+				scanf("%d", &mynumber);
+				if (soBiMat>mynumber)
+				{
+					printf("Oh noo, Bigger!!\n\n");
+				}
+				else if (soBiMat<mynumber)
+				{
+					printf("Oh noo, Smaller!!\n\n");
+				}
+				else
+				{
+					printf("\n\n\nCongratulations!!!\n");
+					printf("You have %d times to win!\n", count);
+					printf("You are winner <3!");
+					count=0;
+				}
 			}
-			else if (soBiMat<mynumber)
+		}
+		else if (nguoichoi==1)
+		{
+			int MAX, MIN, level;
+			MIN=1;
+			printf("All you have to do what guess a number until correct.\n\n");
+			printf("!!LEVEL!!\n\n");
+			printf("1. Level 1 (1-100)\n");
+			printf("2. Level 2 (1-1000)\n");
+			printf("3. Level 3 (1-100000)\n");
+			printf("Your choice: ");
+			scanf("%d", &level);
+			switch(level)
 			{
-				printf("Oh noo, Smaller!!\n\n");
-			}
-			else
-			{
-				printf("\n\n\nCongratulations!!!\n");
-				printf("You have %d times to win!\n", count);
-				printf("You are winner <3!");
-				count=0;
+				case 1:
+					MAX=100;
+					break;
+				case 2:
+					MAX=1000l;
+					break;
+				case 3:
+					MAX=100000;
+					break;
+			}	
+			printf("Gud luck!\n\n\n");
+			soBiMat=(rand()%(MAX-MIN+1))+MIN;
+			while (mynumber!=soBiMat)
+			{	
+				count++;
+				printf("Your number: ");
+				scanf("%d", &mynumber);
+				if (soBiMat>mynumber)
+				{
+					printf("Oh noo, Bigger!!\n\n");
+				}
+				else if (soBiMat<mynumber)
+				{
+					printf("Oh noo, Smaller!!\n\n");
+				}
+				else
+				{
+					printf("\n\n\nCongratulations!!!\n");
+					printf("You have %d times to win!\n", count);
+					printf("You are winner <3!");
+					count=0;
+				}
 			}
 		}
 		printf("\n\nDo you want to play again?\n");
